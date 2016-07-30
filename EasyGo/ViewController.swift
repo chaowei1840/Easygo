9ce42fc8abd64603a2fa200539a855ab5dcc4965
魏超 <c_chao1840@hotmail.com>
2016-07-30 21:43:57 +0800

    Initial Commit

---
+++ EasyGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyGo
+//
+//  Created by 魏超 on 16/7/30.
+//  Copyright © 2016年 魏超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
