f019edf42e8efe666b8f66d954b035dee841a69c
魏超 <c_chao1840@hotmail.com>
2016-08-16 22:22:38 +0800

    增加网络请求失败的处理

--- EasyGo/ViewController.swift
+++ EasyGo/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     let errPlaceholder = "答案错误，再来一次"
     let okPlaceholder = "输入答案"
     let againPlaceholder = "再来一次"
-    let coreSession:URLSession! = URLSession.shared
+    
+    var config:URLSessionConfiguration = URLSessionConfiguration.default
+    
+    var coreSession:URLSession!
     let url = URL(string: "http://192.168.1.108:8080/GradleProject/getNextItem")
     
     override func viewDidLoad() {
@@ -22,6 +25,9 @@ class ViewController: UIViewController {
         label.textAlignment = .center
         answer.text = nil
         answer.placeholder = self.okPlaceholder
+        
+        config.timeoutIntervalForRequest = 4
+        coreSession = URLSession.init(configuration: config)
                 // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -35,6 +41,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var answer: UITextField!
     
     @IBOutlet weak var submit: UIButton!
+    @IBOutlet weak var cover: UILabel!
     
     @IBOutlet weak var desc: UILabel!
     @IBAction func submit(_ sender: AnyObject) {
@@ -80,8 +87,8 @@ class ViewController: UIViewController {
         
         
         let item = Item();
-        item.desc = "123"
-        item.key = "cc"
+        item.desc = "你看到我表示网络请求失败"
+        item.key = "失去连接"
         item.score = 1
         
         
@@ -90,6 +97,11 @@ class ViewController: UIViewController {
         
             let task = coreSession.dataTask(with: url!, completionHandler: { (data, response, error) in
                 self.coreSession.finishTasksAndInvalidate()
+                if(data == nil)
+                {
+                    semaphore.signal()
+                    return
+                }
                 var json:AnyObject
                 do
                 {
@@ -111,7 +123,8 @@ class ViewController: UIViewController {
                 
                 
             })
-                    
+        
+        
             task.resume()
         
             
